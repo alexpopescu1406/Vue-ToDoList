60aca39151c5f541a28623ac3e7789c4552fcfbd
Alexandru Popescu <alexandrupopescu365@gmail.com>
2022-07-13 09:44:57 +0300

    added

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <h1>To-Do List</h1>
+    <ToDoForm></ToDoForm>
+    <ul>
+      <li v-for="item in ToDoItems" :key="item.id">
+        <ToDoItem :label="item.label" v-bind:done="item.done" :id="item.id"></ToDoItem>
+      </li>
+    </ul>
+    </div>
+</template>
+
+<script>
+import ToDoItem from './components/ToDoItem.vue';
+import uniqueId from 'lodash.uniqueid';
+import ToDoForm from './components/ToDoForm.vue';
+
+export default {
+  name: 'App',
+  components: {
+    ToDoItem, 
+    ToDoForm
+  },
+  
+  data() {
+    return {
+      ToDoItems: [
+        { id: uniqueId('todo-'), label: 'aasdasdasdasd', done: true },
+        { id: uniqueId('todo-'), label: 'baaaaaaaa', done: false},
+        { id: uniqueId('todo-'), label: 'ccccccccc', done: true},
+        { id: uniqueId('todo-'), label: 'ddddddddddd', done: true}
+      ]
+    };
+  }
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,22 @@
+    <template>
+      <form>
+        <label for="new-todo input">
+            what to input
+        </label>
+        <input
+            type="text"
+            id="new-todo-input"
+            name="new-todo"
+            autocomplete="off"
+        />
+        <button type="submit">
+            Add
+        </button>
+      </form>
+    </template>
+
+    <script>
+        export default {
+            
+        };
+    </script>
